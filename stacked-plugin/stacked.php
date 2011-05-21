03684dae31bc4bd94b0f8ba027cd10369afb5cb3
Eric Mann <eric@eamann.com>
2011-05-20 20:07:29 -0700

    Very empty version of Stacked plugin

---
+++ stacked-plugin/stacked.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Stacked Plugin
+Plugin URI: http://www.jumping-duck.com
+Description: Adds custom post types and shortcodes for the Stacked Theme.
+Version: 1.0
+Author: Eric Mann
+Author URI: http://www.eamann.com
+License: GPL3
+*/
+
+add_filter( 'stacked-installed', '__return_true' );
+
+?>
--- stacked-theme/functions.php
+++ stacked-theme/functions.php
@@ -1,3 +1,12 @@
 <?php
+if ( ! apply_filters( 'stacked-installed', false ) )
+	add_action( 'admin_notices', 'stacked_plugin_not_installed' );
+
+function stacked_plugin_not_installed() {
+	echo '<div id="message" class="error">
+		<p>This theme requires the Stacked Plugin for proper operation.  Please install it to ensure proper functionality!</p>
+	</div>';
+}
+
 /** Start the engine **/
 require_once(TEMPLATEPATH.'/lib/init.php');
