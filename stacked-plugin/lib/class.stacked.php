88524113b12f65423c990247fea79f77854e5ded
Eric Mann <eric@eamann.com>
2011-05-20 20:35:23 -0700

    Adding custom post type for people

---
+++ stacked-plugin/lib/class.stacked.php
@@ -0,0 +1,37 @@
+<?php
+class Stacked {
+	public static function register_people() {
+		$labels = array(
+			'name' => __('People', 'stacked'),
+			'singular_name' => __('Person', 'stacked'),
+			'add_new' => __('Add New', 'stacked'),
+			'add_new_item' => __('Add New Person', 'stacked'),
+			'edit_item' => __('Edit Person', 'stacked'),
+			'new_item' => __('New Person', 'stacked'),
+			'view_item' => __('View Person', 'stacked'),
+			'search_items' => __('Search People', 'stacked'),
+			'not_found' =>  __('No people found', 'stacked'),
+			'not_found_in_trash' => __('No people found in Trash', 'stacked'),
+			'parent_item_colon' => '',
+			'menu_name' => 'People'
+		);
+
+		$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'query_var' => true,
+			'rewrite' => true,
+			'capability_type' => 'post',
+			'has_archive' => false,
+			'hierarchical' => false,
+			'menu_position' => 5,
+			'menu_icon' => STACKED_PLUGIN_DIR . '/images/people.png',
+			'supports' => array('title','editor','author','thumbnail','excerpt','comments')
+		);
+
+		register_post_type('person',$args);
+	}
+}
--- stacked-plugin/stacked.php
+++ stacked-plugin/stacked.php
@@ -11,4 +11,10 @@ License: GPL3
 
 add_filter( 'stacked-installed', '__return_true' );
 
+if( ! defined('STACKED_PLUGIN_DIR') )
+	define( 'STACKED_PLUGIN_DIR', WP_PLUGIN_URL . '/stacked-plugin' );
+
+require_once('/lib/class.stacked.php');
+
+add_action( 'init', 'Stacked::register_people' );
 ?>
