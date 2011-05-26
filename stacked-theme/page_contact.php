c8c9117d6b23ce27f6d9b3df8600a63bc343fc7a
Eric Mann <eric@eamann.com>
2011-05-25 17:27:42 -0700

    Update contact form

--- stacked-theme/page_contact.php
+++ stacked-theme/page_contact.php
@@ -7,13 +7,13 @@
  */
 
 if(!empty($_POST['send_mail']) && empty($_POST['e-mail'])) {
-	$to = get_option('admin_email');
+	$to = get_bloginfo('admin_email');
 	$subject = "Contact Form Submission";
 
 	$message = "Message from your website:\n\n";
 	$message .= "From: " . stripslashes($_POST["cfname"]) . "\n";
 	$message .= "Email: " . $_POST["cfemail"] . "\n";
-	$message .= "Phone: " . $_POST["cfphone"] . "\n";
+	$message .= "Organization: " . $_POST["cforg"] . "\n";
 	$message .= "Message: " . stripslashes($_POST["cfmessage"]) . "\n\n";
 	$message .= "IP Address: " . $_SERVER["REMOTE_ADDR"] . "\n\n";
 	$message .= "Sent from: " . $_SERVER['HTTP_HOST'] . "\n\n";
@@ -36,11 +36,29 @@ if(!empty($_POST['send_mail']) && empty($_POST['e-mail'])) {
 <p id="message" class="hidden"></p>
 <div class="contactForm">
 	<form method="post" target="_self" action="" onsubmit="javascript:return validate(this);">
-		<div class="field"><label>Your Name</label><input type="text" name="cfname" class="textbox" /></div>
-		<div class="field"><label>Your Email</label><input type="text" name="cfemail" class="textbox" /></div>
-		<div class="field"><label>Your Phone</label><input type="text" name="cfphone" class="textbox" /></div>
-		<div class="field"><label>Message</label><textarea cols="5" rows="5" class="textarea" name="cfmessage" tabindex="4">Enter your message...</textarea></div>
-		<div class="field"><input type="submit" value="SEND MESSAGE" class="submit" name="send_mail" class="button" /></div>
+		<table>
+			<tr>
+			<td>
+				<div class="field"><label>Name</label><input type="text" name="cfname" class="textbox" /></div>
+			</td>
+			<td>
+				<div class="field"><label>Email</label><input type="text" name="cfemail" class="textbox" /></div>
+			</td>
+		</tr>
+		<tr>
+		<td valign="top">
+			<div class="field"><label>Organization</label><input type="text" name="cforg" class="textbox" /></div>
+		</td>
+		<td>
+			<div class="field"><label>Tell us what you're looking for</label><textarea cols="5" rows="5" class="textarea" name="cfmessage" tabindex="4"></textarea></div>
+		</td>
+		</tr>
+		<tr align="center">
+			<td colspan="2">
+				<div class="field"><input type="submit" value="SEND MESSAGE" class="submit" name="send_mail" class="button" /></div>
+			</td>
+		</tr>
+	</table>
 		<input type="hidden" name="e-mail" />
 	</form>
 </div>
