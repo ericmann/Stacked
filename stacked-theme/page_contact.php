148f8a5683721c90cd94376c43f92dd3ac46e25b
Eric Mann <eric@eamann.com>
2011-05-26 17:00:16 -0700

    Fixed contact form.

--- stacked-theme/page_contact.php
+++ stacked-theme/page_contact.php
@@ -26,39 +26,55 @@ if(!empty($_POST['send_mail']) && empty($_POST['e-mail'])) {
 }
 
 ?>
+<div <?php post_class(); ?>>
 
-<?php if($sent == true) { ?>
-	<h2>Thank You!</h2>
-	<p><strong>Your message has been sent.</strong></p>
-<?php }else{ ?>
-	<h2>Contact Form</h2>
-<?php } ?>
-<p id="message" class="hidden"></p>
-<div class="contactForm">
-	<form method="post" target="_self" action="" onsubmit="javascript:return validate(this);">
-		<table>
-			<tr>
-			<td>
-				<div class="field"><label>Name</label><input type="text" name="cfname" class="textbox" /></div>
-			</td>
-			<td>
-				<div class="field"><label>Email</label><input type="text" name="cfemail" class="textbox" /></div>
-			</td>
-		</tr>
-		<tr>
-		<td valign="top">
-			<div class="field"><label>Organization</label><input type="text" name="cforg" class="textbox" /></div>
-		</td>
-		<td>
-			<div class="field"><label>Tell us what you're looking for</label><textarea cols="5" rows="5" class="textarea" name="cfmessage" tabindex="4"></textarea></div>
-		</td>
-		</tr>
-		<tr align="center">
-			<td colspan="2">
-				<div class="field"><input type="submit" value="SEND MESSAGE" class="submit" name="send_mail" class="button" /></div>
-			</td>
-		</tr>
-	</table>
-		<input type="hidden" name="e-mail" />
-	</form>
-</div>
+	<?php do_action( 'genesis_before_post_title' ); ?>
+	<?php do_action( 'genesis_post_title' ); ?>
+	<?php do_action( 'genesis_after_post_title' ); ?>
+
+	<?php do_action( 'genesis_before_post_content' ); ?>
+
+	<div class="entry-content">
+		<?php if($sent == true) { ?>
+		<h2>Thank You!</h2>
+		<p><strong>Your message has been sent.</strong></p>
+		<?php }else{ ?>
+		<?php the_content(); ?>
+		<?php } ?>
+		<p id="message" class="hidden"></p>
+		<div class="contactForm">
+			<form method="post" target="_self" action="" onsubmit="javascript:return validate(this);">
+				<table>
+					<tr>
+					<td>
+						<div class="field"><label>Name</label><input type="text" name="cfname" class="textbox" /></div>
+					</td>
+					<td>
+						<div class="field"><label>Email</label><input type="text" name="cfemail" class="textbox" /></div>
+					</td>
+				</tr>
+				<tr>
+				<td valign="top">
+					<div class="field"><label>Organization</label><input type="text" name="cforg" class="textbox" /></div>
+				</td>
+				<td>
+					<div class="field"><label>Tell us what you're looking for</label><textarea cols="5" rows="5" class="textarea" name="cfmessage" tabindex="4"></textarea></div>
+				</td>
+				</tr>
+				<tr align="center">
+					<td colspan="2">
+						<div class="field"><input type="submit" value="SEND MESSAGE" class="submit" name="send_mail" class="button" /></div>
+					</td>
+				</tr>
+			</table>
+				<input type="hidden" name="e-mail" />
+			</form>
+		</div>
+	</div>
+<?php do_action( 'genesis_after_post_content' ); ?>
+
+</div><!-- end .postclass -->
+<?php
+
+do_action( 'genesis_after_post' );
+?>
