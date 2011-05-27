5beddbc944b3105f73aea71e7b3ecab47fb8b732
Eric Mann <eric@eamann.com>
2011-05-26 21:17:31 -0700

    Lock navigation in place

--- stacked-theme/scripts/stacked.js
+++ stacked-theme/scripts/stacked.js
@@ -1,8 +1,18 @@
 (function($){
 	$.fn.scrollingDiv = function() {
-		var $this = this;
-		$(window).scroll(function(){
-			$this.css( {"top": ($(window).scrollTop()) + "px"} );
+		var $this = this,
+			$window = $(window);
+
+		if(typeof $this.attr('pos') == 'undefined')
+			$this.attr('pos', $this.offset().top);
+
+		$window.scroll(function(){
+			var pos = $window.scrollTop() - $this.attr('pos');
+			if(pos <= -10) {
+				$this.css('position', 'relative');
+			} else {
+				$this.css('position', 'fixed');
+			}
 		});
 	}
 })(jQuery);
